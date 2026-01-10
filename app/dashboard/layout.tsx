9a8f2e6ef9bfd631493893e476dbeea4537564d7
jrgeralde <jrgeralde@gmail.com>
2026-01-11 03:43:20 +0800

    withloginpageguardinlayout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,17 @@
 import Header, { Footer } from './headerfooter';
-import React from 'react';
 
+import React from 'react';
+import LoginPageGuard from '@/components/LoginPageGuard';
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow pt-16 pb-16">
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <LoginPageGuard>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow pt-16 pb-16">
+          {children}
+        </main>
+        <Footer />
+      </div>
+      </LoginPageGuard>
   );
 }
